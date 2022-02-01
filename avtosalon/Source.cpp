dd94e17f50e628a22fab2c960ce937ad42eacebe
Anna <=anna.makukh.99@gmail.com>
2022-02-01 14:58:53 +0200

    added menu

--- avtosalon/Source.cpp
+++ avtosalon/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class car {
@@ -30,7 +31,23 @@ public:
 		cout << "Price : " << price << "\n";
 	}
 };
+int menu()
+{
+	int choice;
+	cout << "0 - Exit\n";
+	cout << "1 - Add car  !\n";
+	cout << "2 - Delete car !\n";
+	cout << "3 - Print car!\n";
+	cout << "4 - Sort !\n";
+	cout << "5 - Search !\n";
+	cout << "Enter your chioce!\n";
+	cin >> choice;
+	return choice;
+}
 int main()
 {
+	vector<car> c = { { "BMW", 2010, 3.5 , 15000 }, { "Audi", 2012, 2 , 13000 } };
+	c.push_back(car{ "Opel", 2008, 1.2 , 8000 });
+
 	return 0;
 }
