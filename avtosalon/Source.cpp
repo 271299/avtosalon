4a289923c134a9f0ca1f79a816e23e586134f682
Anna <=anna.makukh.99@gmail.com>
2022-02-02 19:44:08 +0200

    added car

--- avtosalon/Source.cpp
+++ avtosalon/Source.cpp
@@ -24,7 +24,7 @@ public:
 		this->engine = engine;
 		this->price = price;
 	}
-	void print() {
+	void print()const {
 		cout << "Car : " << name << "\n";
 		cout << "Year : " << year << "\n";
 		cout << "Engine : " << engine << "\n";
@@ -44,10 +44,50 @@ int menu()
 	cin >> choice;
 	return choice;
 }
+void print_vector(const vector<car> c)
+{
+	cout << string(20, '-') << "\n";
+	for (int i = 0; i < c.size(); i++)
+	{
+		
+		c[i].print();
+		cout << string(20, '-')<<"\n";
+	}
+}
 int main()
 {
 	vector<car> c = { { "BMW", 2010, 3.5 , 15000 }, { "Audi", 2012, 2 , 13000 } };
-	c.push_back(car{ "Opel", 2008, 1.2 , 8000 });
-
+	string name;
+	int year;
+	double price, engine;
+    int choice;
+    do {
+		system("cls");
+		print_vector(c);
+        choice = menu();
+        if (!choice)break;
+        switch (choice)
+        {
+        case 1:
+			cout << "Enter car : ";
+			cin >> name;
+			cout << "Enter year : ";
+			cin >> year;
+			cout << "Enter engine : ";
+			cin >> engine;
+			cout << "Enter price : ";
+			cin >> price;
+			c.push_back(car{ name, year, engine , price });
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        }
+    } while (true);
 	return 0;
 }
